3615242605fc36fa787b40b6f2d23416a0c85120
Daniel Mattox <dmattox10@gmail.com>
2019-02-18 23:50:13 -0500

    Undid accidental delete

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,23 @@ module.exports = {
                         loader: "html-loader"
                     }
                 ]
-            }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  "style-loader",
+                  {
+                    loader: "css-loader",
+                    options: {
+                      modules: true
+                    }
+                  }
+                ]
+              },
+              {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+        }
         ]
     },
     plugins: [
