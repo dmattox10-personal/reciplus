a5653bf2605c36631af809294d9815c71da17b24
Daniel Mattox <dmattox10@gmail.com>
2020-04-04 19:37:40 -0400

    Entry.js converted to functional component but crashing

--- client/src/components/Entry.js
+++ client/src/components/Entry.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { cuid } from 'cuid'
+
 import { enter } from '../actions/enter'
+
 import Ingredient from './Ingredient'
 import Instruction from './Instruction'
 import Tag from './Tag'
@@ -10,19 +13,19 @@ const Entry = props => {
     
     const [recipe, updateRecipe] = useState({
         title: '',
-        ingredient: '',
         ingredients: [],
-        ingredientText: '',
-        instruction: '',
         instructions: [],
-        instructionText: '',
         tags: [],
-        tagText: '',
-        user: '',
-        description: '',
+        user: props.user, // was an empty string 
+        description: null, // was an empty string
         file: null
     })
 
+    const [ingredient, updateIngredient] = useState('Enter single ingredient, click add')
+    const [instruction, updateInstruction] = useState('Enter single instruction, click add')
+    const [tag, updateTag] = useState('Enter single tag, click add')
+    const [user, updateUser] = useState(props.auth.user) // Can I do this?
+    
     const handleInputChange = e => {
         e.preventDefault()
         updateRecipe({
@@ -32,154 +35,135 @@ const Entry = props => {
 
     const handleSubmit = e => {
         e.preventDefault();
-        const recipeForm = new FormData()
-        recipeForm.append('title', recipe.title)
-        recipeForm.append('ingredients', recipe.ingredients)
-        recipeForm.append('instructions', recipe.instructions)
-        recipeForm.append('tags', recipe.tags)
-        recipeForm.append('user', recipe.user)
-        recipeForm.append('description', recipe.description)
-        recipeForm.append('file', recipe.file)
+        const recipeForm = new FormData(recipe) // Can I do this?
+        // recipeForm.append('title', recipe.title)
+        // recipeForm.append('ingredients', recipe.ingredients)
+        // recipeForm.append('instructions', recipe.instructions)
+        // recipeForm.append('tags', recipe.tags)
+        // recipeForm.append('user', recipe.user)
+        // recipeForm.append('description', recipe.description)
+        // recipeForm.append('file', recipe.file)
         props.enter(recipeForm);
         props.history.push('/app/my')
     }
 
-    updateIngredientText(e)
+    const createIngredient = e =>
     {
-        this.setState({
-        ingredientText: e.target.value })
-
+        //e.preventDefault();
+        const obj = {
+            ingredient: ingredient,
+            id: cuid()
+        }
+        updateRecipe({
+            ...recipe.ingredients, obj
+        })
+        updateIngredient('')
     }
 
-    createIngredient(e)
+    const createInstruction = e =>
     {
-        e.preventDefault();
-        this.setState({
-        ingredients: [...this.state.ingredients, this.state.ingredientText],
-        ingredientText: '' });
-
+        //e.preventDefault()
+        const obj = {
+            instruction: instruction,
+            id: cuid()
+        }
+        updateRecipe({
+            ...recipe.instructions, obj
+        })
+        updateInstruction('')
     }
 
-    updateInstructionText(e)
+    const createTag = e =>
     {
-        this.setState({
-        instructionText: e.target.value });
-
+        // e.preventDefault()
+        const obj = {
+            tag: tag,
+            id: cuid()
+        }
+        updateRecipe({
+            ...recipe.tags, obj
+        })
+        updateTag('')
     }
 
-    createInstruction(e)
-    {
-        e.preventDefault()
-        this.setState({
-        instructions: [...this.state.instructions, this.state.instructionText],
-        instructionText: '' })
-
-    }
-
-    updateTagText(e)
-    {
-        this.setState({
-        tagText: e.target.value })
-
-    }
-
-    createTag(e)
-    {
-        e.preventDefault()
-        this.setState({
-        tags: [...this.state.tags, this.state.tagText],
-        tagText: '' })
-
-    }
-
-    fileChangedHandler = event => {
-        this.setState({ 
+    const fileChangedHandler = event => {
+        updateRecipe({
             file: event.target.files[0]
         })
-    
-    }      
-
-    componentDidMount() {
-        if(this.props.auth.isAuthenticated) {
-            this.setState({
-                user: this.props.auth.user
-            })
-        }
     }
-
   
-    const { isAuthenticated, user } = this.props.auth
-    const { ingredients, instructions, tags } = this.state
+    const { isAuthenticated } = props.auth
+
     if (isAuthenticated) {
     return (
         <div className="container-fluid">
             <div className="bg">
             <h2 style={{marginBottom: '40px'}}>Add Recipe</h2>
                 <div className="form-group">
-                    <form onSubmit={ this.handleSubmit }>
+                    <form onSubmit={ handleSubmit }>
                         <input
                             placeholder="Catchy Title (Required)"
                             className="form-control"  
                             name="title"
-                            onChange={ this.handleInputChange }
-                            value={ this.state.title }
+                            onChange={ handleInputChange }
+                            value={ recipe.title }
                             /><br />
                         <textarea
                             placeholder="Short description of the dish or recipe (Required)"
                             className="form-control"
                             name="description"
-                            onChange={ this.handleInputChange }
-                            value={ this.state.description }
+                            onChange={ handleInputChange }
+                            value={ recipe.description }
                             /><br />
                             <div className="embed-add">
                                 <input className="form-control"
                                 id="ingredient" 
                                 placeholder="Single ingredient, click add (Required)"
                                 name="ingredient"
-                                onChange={ this.updateIngredientText }
-                                value={ this.state.ingredientText }
-                                /><button className="btn btn-success add" onClick={ this.createIngredient }>Add</button><br />
+                                onChange={ updateIngredient }
+                                value={ ingredient }
+                                /><button className="btn btn-success add" onClick={ () => createIngredient() }>Add</button><br />
                             </div>            
                             <div className="embed-add">                
                                 <input className="form-control"
                                 id="instruction" 
                                 placeholder="Single step, click add (Required)"
                                 name="instruction"
-                                onChange={ this.updateInstructionText }
-                                value={ this.state.instructionText }
-                                /><button className="btn btn-success add" onClick={ this.createInstruction }>Add</button><br />
+                                onChange={ updateInstruction }
+                                value={ instruction }
+                                /><button className="btn btn-success add" onClick={ createInstruction }>Add</button><br />
                             </div>
                             <div className="embed-add">                
                                 <input className="form-control"
                                 id="tag" 
                                 placeholder="Single tag, click add"
                                 name="tag"
-                                onChange={ this.updateTagText }
-                                value={ this.state.tagText }
-                                /><button className="btn btn-success add" onClick={ this.createTag }>Add</button>
+                                onChange={ updateTag }
+                                value={ tag }
+                                /><button className="btn btn-success add" onClick={ createTag }>Add</button>
                             </div>
                             <div className="box">
                             <h2>Ingredients:</h2>
                                 <ul>
-                                    {ingredients.map((ingredient, i) =>
+                                    {recipe.ingredients.map((ingredient, i) =>
                                     <div><Ingredient key={ i + 50 } ingredient={ ingredient }></Ingredient></div>)}
                                 </ul>
                             </div>
                             <div className="box">
                             <h2>Instructions:</h2>
                                 <ul>
-                                    {instructions.map((instruction, i) =>
+                                    {recipe.instructions.map((instruction, i) =>
                                         <div><Instruction key={ i + 10 } instruction={ instruction }></Instruction></div>)}
                                 </ul>
                             </div>
                             <div className="box">
                             <h2>Tags: </h2><p>e.g. Chinese, Spicy, Gluten Free</p>
                                 <ul>
-                                    {tags.map((tag, i) =>
+                                    {recipe.tags.map((tag, i) =>
                                         <div><Tag key={ i + 90 } tag={ tag }></Tag></div>)}
                                 </ul>
                             </div>
-                            <input type="file" name="file" onChange={this.fileChangedHandler}/>
+                            <input type="file" name="file" onChange={fileChangedHandler}/>
                         <button type="submit" className="btn btn-success"> Create Recipe as { user.name }</button>
                     </form>
                 </div>
@@ -196,7 +180,7 @@ const Entry = props => {
     }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
     auth: state.auth,
     user: state.auth.user,
 })
